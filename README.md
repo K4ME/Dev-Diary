6d100d22a5d4f71064a3b480e78ddab9c3b68f62
K4ME <guilhermegodencio@gmail.com>
2023-07-26 14:06:51 -0300

    FEAT - Create Login page

---
+++ README.md
@@ -0,0 +1,1 @@
+logo color: #28ca4b
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useEffect } from 'react'
 import { signInWithGithub } from './configs/authMethods'
+import { ConfigProvider , Button, Row, Col } from 'antd'
+import {GithubOutlined} from '@ant-design/icons'
+import logo from './assets/logo-devdiary.svg'
 
 function App() {
   return (
     <>
-      <header>
-        <button onClick={signInWithGithub}>Logar com github</button>
-      </header>
+    <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#00b96b',
+      },
+    }}
+  > 
+      <body>
+        <img src={logo}/>
+        <p>Bem vindo ao Dev Diary!</p>
+
+        <Button type='primary' onClick={signInWithGithub}> 
+          <GithubOutlined /> Logar com Github
+        </Button>
+      </body>
+
+     </ConfigProvider> 
     </>
   )
 }
